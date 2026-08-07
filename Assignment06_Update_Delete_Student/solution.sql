4befc6cf95a9f04926d859f5917904417553753b
mohanprasanth666 <mohansathish331@gmail.com>
2026-08-07 09:45:09 +0530

    Implement update and delete for student records
    Added update and delete operations for student records.

--- Assignment06_Update_Delete_Student/solution.sql
+++ Assignment06_Update_Delete_Student/solution.sql
@@ -18,8 +18,11 @@ VALUES
 (1002,'Divya','Female',102),
 (1003,'Karthik','Male',101);
 
--- Update Karthik's DepartmentID
+UPDATE Student
+SET DepartmentID = 103
+WHERE StudentName = 'Karthik'
+  AND DepartmentID = 101;
 
--- Delete StudentID 1002
-
--- Display all records
+DELETE FROM Student
+WHERE StudentID = 1002;
+SELECT * FROM Student;
