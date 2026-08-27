11b3a301e83dbc7a0e05b76fe7045f727b6cb75c
mohanprasanth666 <mohansathish331@gmail.com>
2026-08-27 11:25:23 +0530

    Update solution.sql

--- Assignment10_Left_Right_Join/solution.sql
+++ Assignment10_Left_Right_Join/solution.sql
@@ -2,12 +2,40 @@ DROP DATABASE IF EXISTS CollegeDB;
 CREATE DATABASE CollegeDB;
 USE CollegeDB;
 
--- Create Course
+CREATE TABLE Course (
+    CourseID INT PRIMARY KEY,
+    CourseName VARCHAR(50),
+    Credits INT
+);
 
--- Create Enrollment
+INSERT INTO Course (CourseID, CourseName, Credits)
+VALUES
+(201, 'Database Systems', 4),
+(202, 'Data Structures', 3),
+(203, 'Mathematics', 3);
 
--- Insert sample records
 
--- LEFT JOIN
+CREATE TABLE Enrollment (
+    EnrollmentID INT PRIMARY KEY,
+    StudentID INT,
+    CourseID INT
+);
 
--- RIGHT JOIN
+INSERT INTO Enrollment (EnrollmentID, StudentID, CourseID)
+VALUES
+(1, 1001, 201),
+(2, 1001, 202),
+(3, 1002, 203),
+(4, 1003, 201);
+
+SELECT Course.CourseID, Course.CourseName,
+       Enrollment.EnrollmentID, Enrollment.StudentID
+FROM Course
+LEFT JOIN Enrollment
+ON Course.CourseID = Enrollment.CourseID;
+
+SELECT Course.CourseID, Course.CourseName,
+       Enrollment.EnrollmentID, Enrollment.StudentID
+FROM Course
+RIGHT JOIN Enrollment
+ON Course.CourseID = Enrollment.CourseID;
